a6181e28394d2ac454ef0ca73adde7d8893ffcd4
Victor Aguilar <vaguilar.93@hotmail.com>
2024-08-27 05:34:43 -0400

    Diseño de la aplicación con CSS y responsive design junto con la funcionalidad de encriptar texto

---
+++ index.js
@@ -0,0 +1,25 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let buscando = document.getElementById("buscando");
+
+    let textoCifrado = texto
+                        .replace(/e/gi, "enter")
+                        .replace(/i/gi, "imes")
+                        .replace(/a/gi, "ai")
+                        .replace(/o/gi, "ober")
+                        .replace(/u/gi, "ufat");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+        buscando.src = "./img/encriptado.png";
+    } else {
+        buscando.src = "./img/buscando.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+}
